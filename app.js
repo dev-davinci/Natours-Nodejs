02914f887a7c83422655065ffb1648b59fd3901f
Muhammad Gamal <dev.muhammad222000@gmail.com>
2021-02-13 17:54:49 +0200

    Tours routes and controllers

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-import express from "express";
+const express = require("express");
+const morgan = require("morgan");
+
+const tourRoutes = require("./routes/tourRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
-const PORT = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Home");
-});
+if (process.env.ENVIRONMENT === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.static(`${__dirname}/public`));
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/api/v1/users", userRoutes);
+
+app.get("/api/v1/tours", tourRoutes);
 
 app.listen(PORT, (req, res) => {
   console.log(`App is running on port: ${PORT}`);
 });
+
+module.exports = app;
